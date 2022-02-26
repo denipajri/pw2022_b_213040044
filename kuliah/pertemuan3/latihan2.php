5fcc838998111de7fd6e4a18039c356313598707
radenicodes <radenii2002@gmail.com>
2022-02-27 00:11:26 +0700

    menyelesaikan tugas pertemuan 3

---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,15 @@
+<?php
+// Pengkondisisan
+// if else
+// if else if else
+// tenary
+// switch
+
+$x = 2;
+if ($x < 20) {
+  echo "benar";
+} elseif ($x == 20) {
+  echo "bingo!";
+} else {
+  echo "salah";
+}
